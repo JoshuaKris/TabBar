2ba152548e29a676c93be13eee7e27f857f32ab7
DECE <joshua.kris@ymail.com>
2019-03-15 14:58:30 +0700

    tabbar

--- app/src/main/java/com/demo/tabbar/MainActivity.java
+++ app/src/main/java/com/demo/tabbar/MainActivity.java
@@ -1,32 +1,45 @@
 package com.demo.tabbar;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
-        TabLayout tabLayout =(TabLayout) findViewById(R.id.pager);
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 4"));
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("NOTIFICATION"));
+        tabLayout.addTab(tabLayout.newTab().setText("HISTORY"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
-        final PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        final Button button = findViewById(R.id.edt_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               startActivity(new Intent(MainActivity.this,EditActivity.class));
+            }
+        });
 
-        viewPager.setAdapter(pagerAdapter);
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -44,5 +57,17 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/demo/tabbar/PagerAdapter.java
+++ app/src/main/java/com/demo/tabbar/PagerAdapter.java
@@ -3,7 +3,6 @@ package com.demo.tabbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
-import android.widget.Adapter;
 
 public class PagerAdapter extends FragmentStatePagerAdapter {
 
@@ -24,12 +23,6 @@ public class PagerAdapter extends FragmentStatePagerAdapter {
             case 1:
                 TabFragment2 tb2 = new TabFragment2();
                 return tb2;
-            case 2:
-                TabFragment3 tb3 = new TabFragment3();
-                return tb3;
-            case 3:
-                TabFragment4 tb4 = new TabFragment4();
-                return tb4;
 
                 default:
                     return null;
